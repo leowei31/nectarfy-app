ce5617981d41f3d96e783319f61b3ad029d853b2
Leo <leo.wei@alumni.ubc.ca>
2022-02-09 13:59:03 -0800

    changed heights of the dashboard and the lists to use a relative size, also added fixed device orientation

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:flutter/services.dart';
+
 import 'package:flutter/material.dart';
 
 import './views/hive_module.dart';
 import './widgets/hive_module/hive_list/add_hive.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MyApp());
 }
 
@@ -15,16 +23,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Nectarfy',
       theme: ThemeData(
-        primaryColor: Color(0xFFE9C46A),
-        accentColor: Color(0xFF264653),
+        primaryColor: const Color(0xFFE9C46A),
+        accentColor: const Color(0xFF264653),
         fontFamily: 'Lato',
-        
       ),
       home: const MyHomePage(title: 'Nectarfy'),
     );
   }
 }
-
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -36,27 +42,35 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   void _addHiveHandler(BuildContext context) {
-    showModalBottomSheet(context: context, builder: (_) {
-      return AddHive(callbackFn: tempHandler);
-    }); 
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return AddHive(callbackFn: tempHandler);
+        });
   }
 
   void tempHandler() {
     return;
   }
-  
+
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final appbarHeight = AppBar().preferredSize.height;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-      body: const HiveModule(),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () => _addHiveHandler(context),
-      )
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        body: SizedBox(
+            height: (mediaQuery.size.height -
+                appbarHeight -
+                mediaQuery.padding.top -
+                mediaQuery.padding.bottom),
+            child: const HiveModule()),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () => _addHiveHandler(context),
+        ));
   }
 }
--- app/lib/widgets/hive_module/hive_list/hive_list_controller.dart
+++ app/lib/widgets/hive_module/hive_list/hive_list_controller.dart
@@ -3,36 +3,43 @@ import 'package:flutter/material.dart';
 class HiveListController extends StatelessWidget {
   final bool allHives;
   final Function callbackFn;
-  
-  const HiveListController({ Key? key, required this.allHives, required this.callbackFn }) : super(key: key);
+
+  const HiveListController(
+      {Key? key, required this.allHives, required this.callbackFn})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(top: 5, bottom: 5),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          IconButton(onPressed: () => callbackFn(), icon: const Icon(Icons.keyboard_arrow_left_outlined)),
-          Container(
-            width: 250,
-            height: 32,
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.1),
-                  spreadRadius: 1,
-                  blurRadius: 0.5,
-                )
-              ]
-            ),
-            child: Center(child: Text(
-              allHives ? 'ALL HIVES' : 'ACTION ITEMS',
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w300),
-            )),
-          ),
-          IconButton(onPressed: () => callbackFn(), icon: const Icon(Icons.keyboard_arrow_right_outlined)),
-    ]));
+        margin: const EdgeInsets.only(top: 5, bottom: 5),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              IconButton(
+                  onPressed: () => callbackFn(),
+                  icon: const Icon(Icons.keyboard_arrow_left_outlined)),
+              Container(
+                width: 250,
+                height: 32,
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.1),
+                        spreadRadius: 1,
+                        blurRadius: 0.5,
+                      )
+                    ]),
+                child: Center(
+                    child: Text(
+                  allHives ? 'ALL HIVES' : 'ACTION ITEMS',
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w300),
+                )),
+              ),
+              IconButton(
+                  onPressed: () => callbackFn(),
+                  icon: const Icon(Icons.keyboard_arrow_right_outlined)),
+            ]));
   }
 }
